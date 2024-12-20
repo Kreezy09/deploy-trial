a92e69445a0dc24ac0c378a1e726922104a60b21
keith <keithkareithi09@gmail.com>
2024-12-20 17:53:21 +0300

    added confidence level

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ CORS(app)
 # Load the model
 model = load_model('maize_disease.h5')
 
-class_labels = ['Corn___Cercospora_leaf_spot Gray_leaf_spot', 'Corn___Common_rust', 'Corn___Northern_Leaf_Blight', 'Corn___healthy']
+class_labels = ['Gray_leaf_spot', 'Common_rust', 'Northern_Leaf_Blight', 'Healthy']
 
 
 # Image preprocessing function
@@ -23,10 +23,9 @@ def preprocess_image(image, target_size):
     image = image.resize(target_size)
     image = np.array(image)
     image = np.expand_dims(image, axis=0)
-    image = image / 255.0  # Normalization, adjust if necessary
+    image = image / 255.0 
     return image
 
-# Endpoint to upload image and get prediction
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
@@ -39,16 +38,18 @@ def predict():
 
     try:
         image = Image.open(io.BytesIO(file.read()))
-        processed_image = preprocess_image(image, target_size=(224, 224))  # Adjust target size as needed
+        processed_image = preprocess_image(image, target_size=(224, 224)) 
 
         prediction = model.predict(processed_image)
-        # predicted_class = np.argmax(prediction, axis=-1)  # Modify based on your output
+        # predicted_class = np.argmax(prediction, axis=-1)
         predicted_class_idx = np.argmax(prediction, axis=-1)[0]
         predicted_class_name = class_labels[predicted_class_idx]
+        predicted_class_confidence = float(np.max(prediction))
         
         return jsonify({
             'prediction_index': int(predicted_class_idx),
-            'prediction_class': predicted_class_name
+            'prediction_class': predicted_class_name,
+            'confidence': predicted_class_confidence
         })
     except Exception as e:
         return jsonify({'error': str(e)}), 500
